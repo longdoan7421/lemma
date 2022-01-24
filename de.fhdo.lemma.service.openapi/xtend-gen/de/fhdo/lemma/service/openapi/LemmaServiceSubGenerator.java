3b4b24155ef1c6d1bb3b3b142650beed4b902fdf
Jonas Sorgalla <jonas.sorgalla@fh-dortmund.de>
2022-01-24 15:04:35 +0100

    Revert "adds experimental build through target platform for tycho with the goal to utilize tychos mvn dependency management"
    This reverts commit 572d27aa593b92b489a052a6d5fc9926482be64e.

--- de.fhdo.lemma.service.openapi/xtend-gen/de/fhdo/lemma/service/openapi/LemmaServiceSubGenerator.java
+++ de.fhdo.lemma.service.openapi/xtend-gen/de/fhdo/lemma/service/openapi/LemmaServiceSubGenerator.java
@@ -7,7 +7,6 @@ import de.fhdo.lemma.data.DataFactory;
 import de.fhdo.lemma.data.DataField;
 import de.fhdo.lemma.data.DataModel;
 import de.fhdo.lemma.data.ListType;
-import de.fhdo.lemma.data.Type;
 import de.fhdo.lemma.data.Version;
 import de.fhdo.lemma.service.ApiOperationComment;
 import de.fhdo.lemma.service.Endpoint;
@@ -795,7 +794,7 @@ public class LemmaServiceSubGenerator {
         throw Exceptions.sneakyThrow(_t);
       }
     }
-    Type _type = importedType.getType();
+    Object _type = importedType.getType();
     boolean _tripleNotEquals = (_type != null);
     if (_tripleNotEquals) {
       return importedType;
@@ -828,7 +827,7 @@ public class LemmaServiceSubGenerator {
         throw Exceptions.sneakyThrow(_t);
       }
     }
-    Type _type = importedType.getType();
+    Object _type = importedType.getType();
     boolean _tripleNotEquals = (_type != null);
     if (_tripleNotEquals) {
       return importedType;
